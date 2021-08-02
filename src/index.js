c9c1944eb6670e0e8fe04c32aed22c86cd0d3d28
Nikita Shpanko <shpancore@gmail.com>
2021-08-02 14:29:25 +0300

    remove default scene

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import Phaser from 'phaser';
-import logoImg from './assets/logo.png';
 
 class MyGame extends Phaser.Scene
 {
@@ -10,21 +9,12 @@ class MyGame extends Phaser.Scene
 
     preload ()
     {
-        this.load.image('logo', logoImg);
+        
     }
       
     create ()
     {
-        const logo = this.add.image(400, 150, 'logo');
-      
-        this.tweens.add({
-            targets: logo,
-            y: 450,
-            duration: 2000,
-            ease: "Power2",
-            yoyo: true,
-            loop: -1
-        });
+        
     }
 }
 
