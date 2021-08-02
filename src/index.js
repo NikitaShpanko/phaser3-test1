5dda110c4f4efffc214283c19a873cdd6ff2d4e9
Nikita Shpanko <shpancore@gmail.com>
2021-08-02 22:02:17 +0300

    roughly translated to matter physics

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import Phaser from 'phaser';
 
-const SPEED_FACTOR = 1;
+const strokeWidth = 5;
+const radius = 100;
+const frictionAir = 0.2;
+const bounce = 0.7;
 
+let circle;
 class MyGame extends Phaser.Scene {
   constructor() {
     super({
       physics: {
-        default: 'arcade',
-        arcade: {},
+        default: 'matter',
+        matter: {},
       },
     });
   }
@@ -17,15 +21,8 @@ class MyGame extends Phaser.Scene {
   }
 
   create() {
-    const strokeWidth = 5;
-    const radius = 100;
     const { width, height } = this.scale.gameSize;
-    const circle = this.physics.add.existing(
-      this.add.circle(width / 2, height / 2, radius, 0xffffff),
-    );
-    circle.body.setBounce(1, 1);
-    //circle.body.setDamping(true);
-    circle.body.setCollideWorldBounds(true);
+    circle = this.add.circle(width / 2, height / 2, radius, 0xffffff);
 
     circle.setStrokeStyle(strokeWidth);
     circle.strokeColor = 0xff0000;
@@ -41,7 +38,6 @@ class MyGame extends Phaser.Scene {
         const newColor = Phaser.Display.Color.RandomRGB();
         circle.fillColor = newColor.color;
         circle.strokeColor = newColor.saturate(100).lighten(100).color;
-        circle.body.setVelocity(0, 0);
       },
       this,
     );
@@ -56,15 +52,44 @@ class MyGame extends Phaser.Scene {
 
     this.input.setDraggable(circle);
 
+    this.matter.world.disableGravity();
+    this.matter.world.setBounds(0, 0, width, height, 32);
+    const circlePhysics = this.matter.add.gameObject(
+      circle,
+      {
+        circleRadius: radius,
+        frictionAir: frictionAir,
+      },
+      false,
+    );
+    circlePhysics.setBounce(bounce);
+
+    //circlePhysics.setCollidesWith()
+    //circlePhysics.setFrictionAir(0.05);
+    // circlePhysics.setDensity()
+
     this.input.on('drag', (p, obj, dragX, dragY) => {
-      if (!obj.body) return;
-      circle.body.setVelocity((dragX - obj.x) * SPEED_FACTOR, (dragY - obj.y) * SPEED_FACTOR);
+      //if (!obj.body) return;
+      if (obj !== circle) return;
+      circlePhysics.setVelocity(dragX - obj.x, dragY - obj.y);
       obj.setData('dragged', true);
     });
 
     this.input.on('dragstart', (p, obj) => {
       obj.setData('dragged', false);
     });
+  }
+
+  update(time, delta) {
+    const { width, height } = this.scale.gameSize;
+    //console.log(time);
+    //if (time % 10) return;
+    //console.log(circle.x, circle.y);
+    if (circle.x < 0 || circle.x > width || circle.y < 0 || circle.y > height) {
+      console.log(circle.x, circle.y);
+      circle.x = width / 2;
+      circle.y = height / 2;
+    }
   }
 }
 
@@ -75,5 +100,4 @@ const config = {
   height: 600,
   scene: MyGame,
 };
-
 const game = new Phaser.Game(config);
